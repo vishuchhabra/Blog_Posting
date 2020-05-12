734ae2118dbb02589d9b526b7fcb212ae6f9aeaf
vishuchhabra <vishuchhabra1016@gmail.com>
2020-05-12 23:21:04 +0530

    all complete

--- scripts/search.js
+++ scripts/search.js
@@ -2,24 +2,52 @@ const blog  =  require('../api/models/blog')
 const express = require('express')
 const mongoose = require('mongoose')
 
+//function for removing the duplicay
+function removeDuplicates(arr) {
+    return Object.keys(arr.reduce((acc, val) => {
+        acc[JSON.stringify(val)] = 1;
+
+        return acc;
+    }, {})).map((val, key, array) => JSON.parse(array[key]))
+} 
+//function for the searching 
 async function searchID(temp)
 {
     var arr = []
     //condition when query is string
     if(temp.key !== undefined) {
        
-        const result2 =await blog.find({
-            $or :[{ category: { $regex: temp.key, $options: "i" }}, //search based on title
-            {title: { $regex:temp.key, $options: "i" }},     //search based on category
-            {tags: { $regex:temp.key, $options: "i" }},       //search based on listin
-            { listIn: { $regex:temp.key, $options: "i" }},        //search based on Tags
-            { "comments.comment": { $regex:temp.key, $options: "i" }}]  // search based on comment 
-        })
-        result2.forEach(item=>
+        //preference first to category
+        const res1 = await blog.find({category:{$regex:temp.key,$options :"i"}})
+        res1.forEach(item=>
         {
              arr.push(item)
         })
+
+        //preferecne second to title
+        const res2 = await blog.find({title:{$regex:temp.key,$options :"i"}})
+        res2.forEach(item=>
+        { 
+           arr.push(item) 
+        })
+
+        //prefence third to tags
+        const res3 = await blog.find({tags:{$regex:temp.key,$options:"i"}})
+        res3.forEach(item=>
+        { 
+            arr.push(item) 
+        })
+
+        //prefeernce fourth to listIn
+        const res4 = await blog.find({listIn:{$regex:temp.key,$options:"i"}})
+        res3.forEach(item=>
+        { 
+                arr.push(item) 
+        })
+
+        uniq = removeDuplicates(arr)
+
     }
-    return arr
+    return uniq
 }
 exports.searchID = searchID

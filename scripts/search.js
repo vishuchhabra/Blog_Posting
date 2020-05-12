df2d978cf1d3a8d8bebb27b54636bc903be6496a
vishuchhabra <vishuchhabra1016@gmail.com>
2020-05-12 21:38:49 +0530

    Search API

--- scripts/search.js
+++ scripts/search.js
@@ -4,31 +4,22 @@ const mongoose = require('mongoose')
 
 async function searchID(temp)
 {
-   var arr = []
-    //search when query is number 
-    if(temp.number !== undefined) {
-        const val =parseInt(temp.number)
-        const result = await  blog.find({$or:[{likes:val},{disLikes:val}]})
-        result.forEach(item=>
-        {
-            arr.push(item)
-        })
-    }
-
+    var arr = []
     //condition when query is string
-    if(temp.string !== undefined) {
+    if(temp.key !== undefined) {
        
         const result2 =await blog.find({
-            $or :[{ title: { $regex: temp.string, $options: "i" }}, //search based on title
-            {category: { $regex:temp.string, $options: "i" }},     //search based on category
-            {listIn: { $regex:temp.string, $options: "i" }},       //search based on listin
-            { tags: { $regex:temp.string, $options: "i" }},        //search based on Tags
-            { "comments.comment": { $regex:temp.string, $options: "i" }}]  // search based on comment 
+            $or :[{ category: { $regex: temp.key, $options: "i" }}, //search based on title
+            {title: { $regex:temp.key, $options: "i" }},     //search based on category
+            {tags: { $regex:temp.key, $options: "i" }},       //search based on listin
+            { listIn: { $regex:temp.key, $options: "i" }},        //search based on Tags
+            { "comments.comment": { $regex:temp.key, $options: "i" }}]  // search based on comment 
         })
-        result2.forEach(item=>
-        {
-            arr.push(item)
-        })
+        // result2.forEach(item=>
+        // {
+        //     arr.push(item)
+        // })
+        arr.push(result2)
     }
     return arr
 }
